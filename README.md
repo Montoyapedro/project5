1d27becedbe329eee4eb3e6f458dc523bab0959e
Pedro montoya <pedro.mondiaz@outlook.com>
2024-08-13 13:16:28 -0600

    se agrego el grafico de dispersion

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # project5
 Proyecto sprint 5
+En este proyecto se crea un sitio web atraves de render.com 
+con el objetivo de crear gráficos (histograma y de dispersion) visalizando los anuncios de los carros dependiendo de su odometro que es la distancia recorrida por el vehiculo
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import streamlit as st
 
 car_data = pd.read_csv('vehicles_us.csv')  # leer los datos
 hist_button = st.button('Construir histograma')  # crear un botón
+scatter_button = st.button('Construir gráfico de dispersion')
 
 if hist_button:  # al hacer clic en el botón
     # escribir un mensaje
@@ -15,3 +16,9 @@ if hist_button:  # al hacer clic en el botón
 
     # mostrar un gráfico Plotly interactivo
     st.plotly_chart(fig, use_container_width=True)
+if scatter_button:
+    st.write('Creación de un gráfico de dispersion para el conjunto de datos de anuncios de venta de coches')
+
+    # crear un gráfico de dispersión
+    fig = px.scatter(car_data, x="odometer", y="price")
+    fig.show()
